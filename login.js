0d90f683a50bc73e41a956675e3d3e4516900b38
diego.herrera <diego.herrera@wundertec.com>
2021-11-16 18:50:19 -0600

    validacion credenciales

--- login.js
+++ login.js
@@ -25,8 +25,33 @@ $( "#login" ).submit(function( event ) {
 	$.each($(this).serializeArray(),function(i,data){
 		data.name == 'email'?json.email = data.value : json.password = data.value
 	})
-	$.post( "https://intercambios-api.herokuapp.com/api/auth/ingresar",json, function( data ) {
+	/*$.post( "https://intercambios-api.herokuapp.com/api/auth/ingresar",json, function( data ) {
+		
+		console.log(data);
 		sessionStorage.setItem( "token", data.token)
 		location.href = 'dashboard.html';
-	});
+		error: function(e) {
+  		  console.log(e);}
+	});*/
+
+	$.ajax({
+		type: "POST",
+		url: "https://intercambios-api.herokuapp.com/api/auth/ingresar",
+		data: json,
+		success: function(data){
+			console.log(data);
+			sessionStorage.setItem( "token", data.token)
+			location.href = 'dashboard.html';
+
+		},	error: function(e) {
+			Swal.fire({
+				icon: "error",
+				title: "Usuario o contraseña incorrectos",
+				text: "asegurate de que ingresaste credenciales válidas",
+				confirmButtonText: "Aceptar",
+			  });
+  		}
+		
+	  });
+
 });
